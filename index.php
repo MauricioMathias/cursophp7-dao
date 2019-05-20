c9d61424b174a0d0eafc63ff6fb907ffe57b48cd
Mauricio_Mathias <mauricioneis15@gmail.com>
2019-05-20 20:05:34 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,15 +2,24 @@
 
 require_once("config.php");
 
+/*// Carrega um usuário
 $user = new Usuario();
-
 $user->loadbyId(3);
 
-echo $user;
-/*$sql = new Sql();
+echo $user;*/
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+// Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo json_encode($usuarios);*/
+// Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("u");
+// echo json_encode($search);
+
+// carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("mauricio", "12345");
+
+echo $usuario;
 
 ?>
