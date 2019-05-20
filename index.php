d75406aeb8116580167f1fd844ac91cba8647a75
Mauricio_Mathias <mauricioneis15@gmail.com>
2019-05-20 20:34:46 -0300

    Inserir e Alterar

--- index.php
+++ index.php
@@ -12,13 +12,28 @@ echo $user;*/
 //$lista = Usuario::getList();
 //echo json_encode($lista);
 
-// Carrega uma lista de usuários buscando pelo login
-// $search = Usuario::search("u");
-// echo json_encode($search);
+/*Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("u");
+echo json_encode($search);
+*/
 
-// carrega um usuário usando o login e a senha
+/* carrega um usuário usando o login e a senha
 $usuario = new Usuario();
 $usuario->login("mauricio", "12345");
+echo $usuario;
+*/
+
+/* Criando um novo usuário 
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;*/
+
+
+$usuario = new Usuario();
+
+$usuario->loadbyId(7);
+
+$usuario->update("professor", "123programacao");
 
 echo $usuario;
 
